691a7daa0e72282dcc0808ad31d9cc6bd6fbf5a5
Robby Sarvis <rjs.app.dev@proton.me>
2024-08-02 16:31:41 -0500

    version tweak

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Bedrock",
     platforms: [
-        .macOS(.v12),
-        .iOS(.v12)
+        .macOS(.v13),
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
