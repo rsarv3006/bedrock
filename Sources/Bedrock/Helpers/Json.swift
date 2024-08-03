b8fd5b5a900bd8ff1ea2c04146cc2d633925d20b
Robby Sarvis <rjs.app.dev@proton.me>
2024-08-03 06:29:43 -0500

    add load json and decoder to helpers

---
+++ Sources/Bedrock/Helpers/Json.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+public struct JsonHelpers {
+    public static let decoder = JSONDecoder()
+    
+    public func loadJSON<T: Codable>(filename: String) -> T? {
+        guard let path = Bundle.main.path(forResource: filename, ofType: "json") else {
+            print("JSON file not found")
+            return nil
+        }
+        
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path))
+            let result = try JsonHelpers.decoder.decode(T.self, from: data)
+            return result
+        } catch {
+            print("Error decoding JSON: \(error)")
+            return nil
+        }
+    }
+}
